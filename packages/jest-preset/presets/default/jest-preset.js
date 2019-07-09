69847e129c439124800ce52ec49fce2ccccf9037
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-07-09 13:22:46 -0400

    Custom presets for jest

---
+++ packages/jest-preset/presets/default/jest-preset.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/stories/",
+    "/packages/webpack-config/",
+    "/packages/common/",
+    "/packages/bootstrap-theme/"
+  ],
+  transform: {
+    "\\.(gql|graphql)$": require.resolve("jest-transform-graphql"),
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": path.resolve(
+      __dirname,
+      "./../../file-mock.js"
+    ),
+    "^.+\\.tsx?$": path.resolve(__dirname, "./../../babel-jest.js")
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)spec)\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFiles: [path.resolve(__dirname, "./../../polyfills.js")],
+  transformIgnorePatterns: ["/node_modules/(?!@franchises).*/"],
+  modulePathIgnorePatterns: ["/__helpers__/"],
+  moduleNameMapper: {
+    "\\.(scss|css|less)$": require.resolve("identity-obj-proxy"),
+    "\\.(yml)": path.resolve(__dirname, "./../../string-proxy.js")
+  }
+};
---
+++ packages/jest-preset/presets/react/jest-preset.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  ...require("./../default/jest-preset"),
+  setupFilesAfterEnv: [path.resolve(__dirname, "./../../setup-jest.js")],
+  testEnvironment: "enzyme",
+  testEnvironmentOptions: {
+    enzymeAdapter: "react16"
+  }
+};
