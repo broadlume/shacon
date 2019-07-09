2d0b09dd9aaf6d80ba6f4fd82788bedbb1ccf231
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-07-09 12:17:22 -0400

    Resolve jest confs properly

--- packages/jest-preset/jest-preset.js
+++ packages/jest-preset/jest-preset.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 function build(config) {
   const jestConfig = {};
-  const setupFiles = [require.resolve("./polyfills")];
+  const setupFiles = [require.resolve(__dirname, "./polyfills")];
 
   if (config && config.react) {
     Object.assign(jestConfig, {
-      setupFilesAfterEnv: [require.resolve("jest-enzyme")],
+      setupFilesAfterEnv: [require.resolve(__dirname, "jest-enzyme")],
       testEnvironment: "enzyme",
       testEnvironmentOptions: {
         enzymeAdapter: "react16"
@@ -26,9 +26,10 @@ function build(config) {
     transform: {
       "\\.(gql|graphql)$": require.resolve("jest-transform-graphql"),
       "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": require.resolve(
+        __dirname,
         "./file-mock.js"
       ),
-      "^.+\\.tsx?$": path.resolve("./babel-jest.js")
+      "^.+\\.tsx?$": path.resolve(__dirname, "./babel-jest.js")
     },
     testRegex: "(/__tests__/.*|(\\.|/)spec)\\.tsx?$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
@@ -38,7 +39,7 @@ function build(config) {
     modulePathIgnorePatterns: ["/__helpers__/"],
     moduleNameMapper: {
       "\\.(scss|css|less)$": require.resolve("identity-obj-proxy"),
-      "\\.(yml)": require.resolve("./string-proxy.js")
+      "\\.(yml)": require.resolve(__dirname, "./string-proxy.js")
     }
   };
 }
