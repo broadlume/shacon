ad156b6429c5e9b66791bdf69816c265cefde8e8
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-07-10 14:57:36 -0400

    Disable optimizing plugins in test env

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -1,33 +1,40 @@
 const { kebabCase } = require("lodash");
 const { declare } = require("@babel/helper-plugin-utils");
 
+const optimizationPlugins = [
+  "babel-plugin-lodash",
+  "babel-plugin-idx",
+  [
+    "babel-plugin-transform-imports",
+    {
+      reactstrap: {
+        transform: "reactstrap/lib/${member}",
+        preventFullImport: true
+      },
+      "@franchises/form-helpers": {
+        transform: importName =>
+          `@franchises/form-helpers/src/${kebabCase(importName)}`,
+        preventFullImport: true,
+        skipDefaultConversion: true
+      }
+    }
+  ]
+];
+
 module.exports = declare((api, _options) => {
-  // see docs about api at https://babeljs.io/docs/en/config-files#apicache
   api.assertVersion("^7.0.0");
 
   return {
     presets: [["react-app", { flow: false, typescript: true }]],
-    plugins: [
-      "babel-plugin-idx",
-      [
-        "babel-plugin-transform-imports",
-        {
-          reactstrap: {
-            transform: "reactstrap/lib/${member}",
-            preventFullImport: true
-          },
-          "@franchises/form-helpers": {
-            transform: importName =>
-              `@franchises/form-helpers/src/${kebabCase(importName)}`,
-            preventFullImport: true,
-            skipDefaultConversion: true
-          }
-        }
-      ]
-    ],
     env: {
+      development: {
+        // We use these in development to catch errors early. However we cannot
+        // use them in the test env because it breaks coverage, amongst other
+        // things.
+        plugins: optimizationPlugins
+      },
       production: {
-        plugins: ["babel-plugin-lodash"]
+        plugins: optimizationPlugins
       }
     }
   };
