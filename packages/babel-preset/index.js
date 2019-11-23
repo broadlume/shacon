dfd29893532d0b229888d09a4ce439199ae19ee9
Ian Ker-Seymer <i.kerseymer@gmail.com>
2019-11-22 19:11:22 -0500

    Ensure optional chaining is setup properly

--- packages/babel-preset/index.js
+++ packages/babel-preset/index.js
@@ -32,7 +32,7 @@ module.exports = declare((api, _options) => {
         { flow: false, typescript: true }
       ]
     ],
-    plugins: ["@babel/plugin-proposal-optional-chaining"],
+    plugins: [require.resolve("@babel/plugin-proposal-optional-chaining")],
     env: {
       development: {
         // We use these in development to catch errors early. However we cannot
